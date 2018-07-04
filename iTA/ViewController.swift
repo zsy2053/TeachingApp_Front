0dcaf6633d7344cc77caf67334fd0f213abbbbfb
Shuyang Zang <shuyangzang@shuyangs-macbook-pro.local>
2018-07-03 22:17:42 -0400

    Initial Commit

---
+++ iTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTA
+//
+//  Created by Shuyang Zang on 2018-07-03.
+//  Copyright © 2018 Shuyang Zang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iTATests/iTATests.swift
@@ -0,0 +1,36 @@
+//
+//  iTATests.swift
+//  iTATests
+//
+//  Created by Shuyang Zang on 2018-07-03.
+//  Copyright © 2018 Shuyang Zang. All rights reserved.
+//
+
+import XCTest
+@testable import iTA
+
+class iTATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
