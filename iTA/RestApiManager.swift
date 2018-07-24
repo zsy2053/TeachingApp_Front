d536a233197a9b0f8ef596fb117f60cff412d62e
Shuyang Zang <shuyangzang@Shuyangs-MacBook-Pro.local>
2018-07-23 23:45:16 -0400

    Added files

--- iTA/RestApiManager.swift
+++ iTA/RestApiManager.swift
@@ -5,5 +5,32 @@
 //  Created by Shuyang Zang on 2018-07-18.
 //  Copyright © 2018 Shuyang Zang. All rights reserved.
 //
-
+import SwiftyJSON
 import Foundation
+typealias ServiceResponse = (JSON, NSError?) -> Void
+class ResetApiManager: NSObject {
+    static let sharedInstance = ResetApiManager()
+    
+    let baseURL = "http://localhost:3001/"
+    
+    func makeHTTPGetRequest(path: String, onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void) {
+        let url = URL(string: path)
+        let request = URLRequest(url: url!)
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request, completionHandler: {data, response, error -> Void in
+            do {
+                if(error != nil) {
+                    onFailure(error!)
+                } else {
+                    let result = try JSON(data: data!)
+                    onSuccess(result)
+                }
+            } catch {
+                print(error)
+            }
+        })
+        task.resume()
+    }
+}
--- iTA/Supportcontrollers/RegisterViewController.swift
+++ iTA/Supportcontrollers/RegisterViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    var user_status = "someString"
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +24,39 @@ class RegisterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func Teacher(_ sender: Any) {
+        user_status = "teacher"
     }
-    */
-
+    @IBAction func Student(_ sender: Any) {
+        user_status = "student"
+    }
+    
+    @IBAction func Register(_ sender: Any) {
+        let baseURL:URL = URL(string: "http://localhost:3001/")!
+        let userName = UserName.text
+        let userEmail = Email.text
+        
+        if (userEmail == nil || userName == nil) {
+            //            Display error message
+            return
+        }
+        
+        //        Store data
+        UserDefaults.standard.set(userEmail, forKey:"userEmail")
+        UserDefaults.standard.set(userEmail, forKey:"userName")
+        UserDefaults.standard.synchronize()
+        Alamofire.request(baseURL.appendingPathComponent("users"), method: .post, parameters: ["username": userName, "email": userEmail, "status": user_status], encoding: JSONEncoding.default, headers: nil).responseJSON (completionHandler: {
+            response in
+            switch response.result {
+            case .success:
+                let alert = UIAlertController(title: "Registration Success", message: "You have successfully registered an iTA account, thank you", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Start studying", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                print(response.result.value)
+                
+            case .failure(let error):
+                print(error)
+            }
+        })
+    }
 }
