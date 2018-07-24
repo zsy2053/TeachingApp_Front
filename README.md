7eba126c72eff1c401d66d47ee43788524ed1fba
Shuyang Zang <shuyangzang@Shuyangs-MacBook-Pro.local>
2018-07-23 23:42:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TeachingApp_Front
---
+++ iTA/RestApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  RestApiManager.swift
+//  iTA
+//
+//  Created by Shuyang Zang on 2018-07-18.
+//  Copyright © 2018 Shuyang Zang. All rights reserved.
+//
+
+import Foundation
