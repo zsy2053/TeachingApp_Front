afb22c9864f1d89766bcc32f69d207dc1ddc978f
Shuyang Zang <shuyangzang@Shuyangs-MacBook-Pro.local>
2018-07-24 22:59:55 -0400

    Added authentication

--- iTA/Supportcontrollers/RegisterViewController.swift
+++ iTA/Supportcontrollers/RegisterViewController.swift
@@ -11,12 +11,20 @@ import Alamofire
 
 class RegisterViewController: UIViewController {
 
-    @IBOutlet weak var UserName: UITextField!
-    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var EmailCheck: UILabel!
+    @IBOutlet weak var Register: UIButton!
     var user_status = "someString"
+    var user_name = ""
+    var user_email = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        user_name = ""
+        user_email = ""
+        Label.text = "Please Enter User Name"
+        EmailCheck.text = "Please Enter User Email"
+        Register.isEnabled = false
         // Do any additional setup after loading the view.
     }
 
@@ -24,35 +32,68 @@ class RegisterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func Teacher(_ sender: Any) {
-        user_status = "teacher"
+    @IBAction func EnterUserName(_ sender: UITextField) {
+        user_name = sender.text!
+        if (user_name.isEmpty) {
+            Label.text = "Please Enter User Name"
+            Register.isEnabled = false
+        } else {
+            Label.text = ""
+            if (EmailCheck.text == "") {
+                Register.isEnabled = true
+            } else {
+                Register.isEnabled = false
+            }
+        }
     }
+ 
+    @IBAction func EnterEmail(_ sender: UITextField) {
+        user_email = sender.text!
+        let __firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
+        let __serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
+        let __emailRegex = __firstpart + "@" + __serverpart + "[A-Za-z]{2,8}"
+        let __emailPredicate = NSPredicate(format: "SELF MATCHES %@", __emailRegex)
+        if (user_email.isEmpty) {
+            EmailCheck.text = "Please Enter User Email"
+            Register.isEnabled = false
+        } else if (!__emailPredicate.evaluate(with: sender.text)) {
+            EmailCheck.text = "Invalid Email Address"
+            Register.isEnabled = false
+        } else {
+            EmailCheck.text = ""
+            if(Label.text == "") {
+                Register.isEnabled = true
+            } else {
+                Register.isEnabled = false
+            }
+        }
+    }
+    
+    
     @IBAction func Student(_ sender: Any) {
         user_status = "student"
     }
     
+    @IBAction func Teacher(_ sender: Any) {
+        user_status = "teacher"
+    }
+    
+    
+    
     @IBAction func Register(_ sender: Any) {
         let baseURL:URL = URL(string: "http://localhost:3001/")!
-        let userName = UserName.text
-        let userEmail = Email.text
-        
-        if (userEmail == nil || userName == nil) {
-            //            Display error message
-            return
-        }
-        
-        //        Store data
-        UserDefaults.standard.set(userEmail, forKey:"userEmail")
-        UserDefaults.standard.set(userEmail, forKey:"userName")
-        UserDefaults.standard.synchronize()
-        Alamofire.request(baseURL.appendingPathComponent("users"), method: .post, parameters: ["username": userName, "email": userEmail, "status": user_status], encoding: JSONEncoding.default, headers: nil).responseJSON (completionHandler: {
+
+        Alamofire.request(baseURL.appendingPathComponent("users"), method: .post, parameters: ["username": user_name, "email": user_email, "status": user_status], encoding: JSONEncoding.default, headers: nil).responseJSON (completionHandler: {
             response in
             switch response.result {
             case .success:
                 let alert = UIAlertController(title: "Registration Success", message: "You have successfully registered an iTA account, thank you", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Start studying", style: .default, handler: nil))
+                alert.addAction(UIAlertAction(title: "Start studying", style: .default, handler: {_ in
+                    let VC = self.storyboard?.instantiateViewController(withIdentifier: "Authentication")
+                    
+                    self.present(VC!, animated: true, completion: nil)
+                }))
                 self.present(alert, animated: true)
-                print(response.result.value)
                 
             case .failure(let error):
                 print(error)
